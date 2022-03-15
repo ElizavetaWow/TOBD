b67896e68c50eab05889d1e6597ddf43852a1666
ElizavetaWow <59148803+ElizavetaWow@users.noreply.github.com>
2022-03-15 12:46:44 +0300

    Renaming

--- sem10/count_letters.py
+++
@@ -1,5 +0,0 @@
-from collections import Counter
-
-def count_letters(file):
-    with open(file, 'r') as fp:
-        return Counter(fp.read().lower())
--- sem10/worker.py
+++
@@ -1,6 +0,0 @@
-from mean_steps_for_file_mp import mean_steps_for_file_mp
-
-def worker(in_q, out_q):
-    for task in iter(in_q.get, 'STOP'):
-        d = mean_steps_for_file_mp(task)
-        out_q.put(d)
